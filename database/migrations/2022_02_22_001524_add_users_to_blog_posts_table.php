cb70c5cb448fe979aeac45ec951c7d45ce1e5272
oxicuza <oxicusa@gmail.com>
2022-05-09 08:56:15 +0700

    create global scope for latest data from db

--- database/migrations/2022_02_22_001524_add_users_to_blog_posts_table.php
+++ database/migrations/2022_02_22_001524_add_users_to_blog_posts_table.php
@@ -14,7 +14,7 @@ class AddUsersToBlogPostsTable extends Migration
     public function up()
     {
         Schema::table('blog_posts', function (Blueprint $table) {
-            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->foreignId('user_id')->constrained('users');
         });
     }
 
